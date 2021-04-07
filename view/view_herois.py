3dd95d229a21ae6894dfbb01a9f97fd6bfc3e1cb
felisouza <felisouza1991@gmail.com>
2021-04-07 18:48:21 -0300

    Implementação das funções para ler o CSV. Com comentários

--- view/view_herois.py
+++ view/view_herois.py
@@ -21,7 +21,7 @@ class View_herois:
             self.mostra_dados_herois(resultado)
         elif resultado is not None and self.flag.lower() == 'csv': #Criei esse elif caso a flag seja CSV
             self.linhas()
-            self.mostra_dados_herois(resultado)
+            self.mostra_dados_herois_csv(resultado)
         
     def mostra_dados_herois(self, lista_herois):
         contador = 1
@@ -34,7 +34,23 @@ class View_herois:
             self.linhas()
             contador += 1
 
-        
+    def mostra_dados_herois_csv(self, lista_herois):
+        contador = 1
+        for heroi in lista_herois:
+            nome_heroi = heroi['name']
+            descricao_heroi = heroi['description']
+
+            print(f'Heroi ({contador})')
+            print(f'Nome heroi: {nome_heroi}')
+            print(f'Descrição: {descricao_heroi}')
+            self.linhas()
+            contador += 1
+            """
+            print(f'Lista comics: {heroi.lista_comics}')
+            print(f'Lista series: {heroi.lista_series}')
+            
+            """
+
 
 
 	    
